08bf123992361b7f873df6a48843884192576a4a
GulityCrown <109987666+GulityCrown@users.noreply.github.com>
2022-10-10 21:12:52 +0200

    Update Numbers.cs

--- Open-Lab-04.12/Numbers.cs
+++ Open-Lab-04.12/Numbers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace Open_Lab_04._12
 {
@@ -6,7 +7,17 @@ namespace Open_Lab_04._12
     {
         public int MissingNum(int[] nums)
         {
-            throw new NotImplementedException();
+            int mN = 0;
+            int sum = 0;
+            foreach(int num in nums)
+            {
+                sum += num;
+
+            }
+            if (sum < 55)
+                mN = 55 - sum;
+            return mN;
+
         }
     }
 }
